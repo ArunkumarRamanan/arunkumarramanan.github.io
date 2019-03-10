cb3313257b9894abaf3ee8f430092124342f44b2
Arunkumar Venkataramanan <arunkumar.ramanan@hotmail.com>
2019-03-10 13:22:55 +0530

    Edited
    Updated

---
+++ js/index.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    $(function(){
+        $("#profile-card-header").typed({
+            strings: ["Data Scientist",
+                      "Machine Learning Enthusiast",
+                      "Reinforcement Learning Engineer",
+                      "Rutgers University",
+                      "Data Science Graduate '20"
+            ],
+            typeSpeed: 0.4,
+            loop: true
+        });
+    });
+
+
+    //Default options
+    var options = {
+        animationDuration: 0.5, //in seconds
+        filter: 'all', //Initial filter
+        callbacks: {
+            onFilteringStart: function() { },
+            onFilteringEnd: function() { },
+            onShufflingStart: function() { },
+            onShufflingEnd: function() { },
+            onSortingStart: function() { },
+            onSortingEnd: function() { }
+        },
+        delay: 0, //Transition delay in ms
+        delayMode: 'progressive', //'progressive' or 'alternate'
+        easing: 'ease-out',
+        filterOutCss: { //Filtering out animation
+            opacity: 0,
+            transform: 'scale(0.5)'
+        },
+        filterInCss: { //Filtering in animation
+            opacity: 1,
+            transform: 'scale(1)'
+        },
+        layout: 'sameSize', //See layouts
+        selector: '.filtr-container',
+        setupControls: true
+    };
+
+    $(function(){
+        $('#tab-3').click(function(){
+            if ($(this).is(':checked'))
+            {
+                var filterizd = $('.filtr-container').filterizr(options);
+            }
+        });
+    });
+
+    $(function(){
+        $('.isotopeFilters2').on( 'click', 'a', function(e) {
+            $('.isotopeFilters2').find('.active').removeClass('active');
+            $(this).parent().addClass('active');
+
+        });
+    });
+
+});
+
+
+
+
